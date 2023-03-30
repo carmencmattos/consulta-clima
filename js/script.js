862ff950d5ad2f68574117084dd9ecd73adcdf72
Carmen Mattos <carmencmattos@gmail.com>
2023-03-30 19:27:05 -0300

    criando uma aplicação de clima

---
+++ js/script.js
@@ -0,0 +1,97 @@
+//Variáveis e seleção de elementos
+const apiKey = "e58826a73392ee6b03a6ef4f576a42cc";
+const apiUnsplash = "https://source.unsplash.com/1600x900/?";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
+
+
+//loader
+const toggleLoader = () => {
+    loader.classList.toggle("hide");
+};
+
+//Funções
+const getWeatherData = async(city) => {
+    toggleLoader();
+
+    const apiWeatherURL =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    toggleLoader();
+
+    return data;
+
+};
+
+//Tratamento de erros
+const showErrorMessage = () => {
+    errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+    errorMessageContainer.classList.add("hide");
+    weatherContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+    hideInformation();
+
+    const data = await getWeatherData(city);
+
+    if (data.cod === "404") {
+        showErrorMessage();
+        return;
+    }
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    );
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    //Alterando a imagem do background
+    document.body.style.backgroundImage = `url("${apiUnsplash + city}")`;
+
+    weatherContainer.classList.remove("hide");
+
+};
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if(e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+
+})
+
+
+
+    
